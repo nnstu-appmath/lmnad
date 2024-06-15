194bf7f8c171bbcbc5ecf329c5105343f2e48618
esinkirill <78916798+esinkirill@users.noreply.github.com>
2024-06-16 01:36:35 +0300

    Update backup.sh

--- configs/backup.sh
+++ configs/backup.sh
@@ -1,34 +1,47 @@
 #!/bin/bash
 
 mysql_user=root
-mysql_password=78910
+mysql_password=${MYSQL_ROOT_PASSWORD}
 
 now=$(date +"%d_%m_%Y")
 
+echo "--------------------------------------------------------------------------------------"
+
 docker exec lmnad_web sh -c 'mkdir -p /root/backups'
-echo "Создана директория для резервных копий на контейнере lmnad_web."
 
 docker exec lmnad_mysql sh -c '
     mkdir -p /root/backups &&
     rm -f /root/backups/* &&
-    mysqldump --user=$1 --password=$2 --triggers --routines --databases lmnad_db | gzip > /root/backups/backup_$3.sql.gz' sh "$mysql_user" "$mysql_password" "$now"
-echo "Выполнен mysqldump и создана сжатая резервная копия базы данных."
+    mysqldump --user=$1 --password=$2 --triggers --routines --databases lmnad_db | gzip > /root/backups/backup_$3.sql.gz ' sh "$mysql_user" "$mysql_password" "$now"
 
-docker cp lmnad_mysql:/root/backups/backup_$now.sql.gz /tmp/
-docker cp /tmp/backup_$now.sql.gz lmnad_web:/root/backups
-echo "Скопирована сжатая резервная копия базы данных на контейнер lmnad_web."
+echo "Performed mysqldump and created compressed backup of the database."
 
-docker exec lmnad_mysql sh -c 'rm -f /root/backups/*'
-echo "Удалены временные файлы резервной копии базы данных на контейнере lmnad_mysql."
+docker cp lmnad_mysql:/root/backups/backup_$now.sql.gz /home/esinkirill/backups/
+
+echo "Copied MySQL backup to /home/esinkirill/backups/."
 
 docker exec lmnad_web sh -c '
     cd /lmnad/project/media &&
-    tar -czf /root/backups/backup.lmnad_uploads_$1.tar.gz uploads/ &&
-    rsync -avz /root/backups/ esinkirill@188.120.225.17:/home/esinkirill/backups/ &&
-    ssh esinkirill@188.120.225.17 "find /home/esinkirill/backups/* -mtime +7 -exec rm {} \;"
-' sh "$now"
-echo "Создана и скопирована сжатая резервная копия медиафайлов, а также удалены старые файлы резервных копий на удаленном хосте."
+    tar -czf /root/backups/backup.lmnad_uploads_$1.tar.gz uploads/ ' sh "$now"
+echo "Created compressed backup of media files."
 
-docker exec lmnad_web sh -c 'rm -f /root/backups/*'
-echo "Удалены временные файлы резервных копий на контейнере lmnad_web."
+docker cp lmnad_web:/root/backups/backup.lmnad_uploads_$now.tar.gz /home/esinkirill/backups/
+echo "Copied media backup to /home/esinkirill/backups/."
+
+docker exec lmnad_mysql sh -c ' rm -f /root/backups/* '
+docker exec lmnad_web sh -c ' rm -f /root/backups/* '
+echo "Removed temporary backup files from lmnad_web and lmnad_mysql containers."
+
+rsync -avz /home/esinkirill/backups/ esinkirill@188.120.225.17:/home/esinkirill/backups/
+echo "Copied backup files to the remote server."
+
+ssh esinkirill@188.120.225.17 "find /home/esinkirill/backups/* -mmin +2820 -exec rm {} \;"
+echo "Removed backup files older than 47 hours from the remote server."
+
+find /home/esinkirill/backups/* -mmin +1380 -exec rm {} \;
+echo "Removed backup files older than 23 hours from the local server."
+
+echo "Backup completed at $(date)" >> /home/esinkirill/backup.log 2>&1
+
+echo "--------------------------------------------------------------------------------------"
 
